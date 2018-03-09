4ae3db9fac0f5127ad0caf8966fd1681f1377994
Mario DeLaPaz <mariocd10@gmail.com>
2018-03-09 11:27:07 -0500

    +keys to the payload object
    added keys to match the createListing form
    updated http.post method to have the correct format. tutorial had
    outdated format.

--- app/scripts/controllers/createListing.js
+++ app/scripts/controllers/createListing.js
@@ -9,14 +9,33 @@
  */
 angular.module('clientApp')
 .controller('createListingCtrl', function ($scope, $http, $log) {
-  $scope.signup = function() {
+	var dt = new Date();
+	var utcDate = dt.toUTCString();
+	$scope.create = function() {
     var payload = {
-      streetName : $scope.streetName,
-      StreetNumber : $scope.streetNumber
+      street_name 	: $scope.streetName,
+      street_number	: $scope.streetNumber,
+      street_type 	: $scope.streetType,
+      apt_number 	: $scope.aptNumber,
+      city 			: $scope.city,
+      state		 	: $scope.state,
+      zip_code 		: $scope.zipCode,
+      description 	: $scope.description,
+      bedrooms 		: $scope.bedrooms,
+      bathrooms 	: $scope.bathrooms,
+      home_square_feet 		: $scope.homeSqft,
+      land_square_feet 		: $scope.landSqft,
+      sale_type 		: $scope.saleType,
+      year_built 		: $scope.yrBuilt,
+      market_date 	: $scope.utcDate,
+      property_type 		: $scope.propType,
+      finished_basement : $scope.finishedBasement,
+      price 		: $scope.price,
+      is_active 		: $scope.isActive
     };
 
     $http.post('app/createListing', payload)
-        .success(function(data) {
+        .then(function(success, data){
           $log.debug(data);
         });
   };
