cfae4376603e22f242fa49d96d22aab1cf551670
Mario DeLaPaz <mariocd10@gmail.com>
2018-07-09 20:39:56 -0400

    update 1 for attach image

--- app/scripts/controllers/createListing.js
+++ app/scripts/controllers/createListing.js
@@ -11,12 +11,7 @@ angular.module('clientApp')
 .controller('createListingCtrl', function ($scope, $http, $log) {
 	
 	var d = new Date();
-	d = d.getFullYear() + "-"
-		+ ('0' + (d.getMonth() + 1)).slice(-2) 
-		+ "-" + ('0' + d.getDate()).slice(-2) 
-		+ " " + ('0' + d.getHours()).slice(-2) 
-		+ ":" + ('0' + d.getMinutes()).slice(-2) 
-		+ ":" + ('0' + d.getSeconds()).slice(-2);
+	d = d.getFullYear() + "-" + ('0' + (d.getMonth() + 1)).slice(-2) + "-" + ('0' + d.getDate()).slice(-2) + " " + ('0' + d.getHours()).slice(-2) + ":" + ('0' + d.getMinutes()).slice(-2) + ":" + ('0' + d.getSeconds()).slice(-2);
 	
 	$scope.create = function() {
     var payload = {
